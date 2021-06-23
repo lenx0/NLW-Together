f7a07498de209668b63af7ced9191431cfac9e71
lenx0 <thiagofreitas1106@gmail.com>
2021-06-22 21:16:58 -0300

    Navegacao entre pagina home e pagina de criacao de sala

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <NewRoom />
+    //exact garante que o endereço seja exatamente o listado para ser acessado,se não colocar isso, ele pode mostrar varias rotas ao mesmo tempo na pagina
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />        
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
